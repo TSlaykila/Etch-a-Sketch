c4ba3fade0d654249b3c90fbf294a7d457504481
TSlaykila <farimar2004@gmail.com>
2025-01-09 13:47:16 -0400

    added feature of the user being able to choose the number of pixel desired throught a button, which updates to a new grid square

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const container = document.querySelector(".container"); 
-const containerWidth = 400;
-document.body.style.cssText = "display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;";
+const containerWidth = 600;
+document.body.style.cssText = "display: flex;flex-direction: column ; justify-content: center; align-items: center; height: 100vh; margin: 0;";
 container.style.cssText = `display: flex;flex-wrap: wrap; justify-content: center; align-items: center; margin: 0; width: ${containerWidth}px`;
 
-for(let i = 0; i < 16*16; i++){
-    container.style.flexDirection ="row";
-    const gridHorizontal = document.createElement("div");
-    gridHorizontal.setAttribute("class", "grid")
-    gridHorizontal.style.border = "0.5px solid black";
-    gridHorizontal.style.boxSizing = "border-box";
-    gridHorizontal.style.height = "25px";
-    gridHorizontal.style.width = "25px";
-    container.appendChild(gridHorizontal);
-};
 
-const grid = document.querySelectorAll(".grid");
-grid.forEach((square) => {
-    square.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "black";
+const squareNumber = document.createElement("button");
+squareNumber.innerHTML = "Square Number";
+squareNumber.addEventListener("click", () => {
+    number = parseInt(prompt("How many squares do you wish per side"));
+    container.innerHTML = ""
+    makeSquare();
 });
-});
+
+document.body.appendChild(squareNumber);
+function makeSquare(){
+    for(let i = 0; i < number*number; i++){
+        container.style.flexDirection ="row";
+        const gridHorizontal = document.createElement("div");
+        gridHorizontal.setAttribute("class", "grid")
+        gridHorizontal.style.border = "0.5px solid black";
+        gridHorizontal.style.boxSizing = "border-box";
+        gridHorizontal.style.height = `${containerWidth / number}px`;
+        gridHorizontal.style.width = `${containerWidth / number}px`;
+        container.appendChild(gridHorizontal);
+    };
+
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach((square) => {
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+    });
+    });
+};
+let number = 16;
+makeSquare();
+
